45aee18fb85d7446e66c9c9cef5c1447791d95a1
DewGit3 <278855476+DewGit3@users.noreply.github.com>
2026-09-13 19:56:55 +0700

    new sites and apps

--- TheOpenSupport.py
+++ TheOpenSupport.py
@@ -7,15 +7,34 @@ print("ru:\nДобро пожаловать в TOS\nВведите имя при
 while True:
     command = input(">> ").lower()
     
+    
     #русская версия  / russian version
-    if command == "хром":
+    #сайты / sites
+    if command == "ютуб":
+        webbrowser.open('www.youtube.com')
+        
+    elif command == "телеграм сайт":
+        webbrowser.open('https://web.telegram.org/')
+        
+    elif command == "гитхаб":
+        webbrowser.open('https://github.com/')
+        
+    elif command == "гитхаб разработчика":
+        webbrowser.open('https://github.com/DewGit3')
+        
+    elif command == "чат гпт":
+        webbrowser.open('https://chatgpt.com/uc/6aa296e2-980c-83ea-9095-e25ceac01998')
+        
+    elif command == "клод":
+        webbrowser.open('https://claude.com/')
+        
+
+    #приложения / apps
+    elif command == "хром":
         os.system("start chrome")
         
     elif command == "фаерфокс":
         os.system("start firefox")
-        
-    elif command == "ютуб":
-        webbrowser.open('www.youtube.com')
         
     elif command == "дискорд":
         os.system("start discord:")
@@ -25,27 +44,12 @@ while True:
         
     elif command == "тг":
         os.system("start tg:")
-        
-    elif command == "телеграм браузер":
-        webbrowser.open('https://web.telegram.org/')
     
     elif command == "стим":
         os.system("start steam:")
         
-    elif command == "гитхаб":
-        webbrowser.open('https://github.com/')
-        
-    elif command == "гитхаб разработчика":
-        webbrowser.open('https://github.com/DewGit3')
-        
     elif command == "консоль":
         os.system("start cmd")
-    
-    elif command == "чат гпт":
-        webbrowser.open('https://chatgpt.com/uc/6aa296e2-980c-83ea-9095-e25ceac01998')
-                
-    elif command == "клод":
-        webbrowser.open('https://claude.com/')
         
     elif command == "калькулятор":
         os.system("start calc")
@@ -55,51 +59,68 @@ while True:
         
     elif command == "проводник":
         os.system("start explorer")
+        
+    elif command == "вс код":  
+        os.system("start code")
     
-    elif command == "clear":
-        break
-    elif command == "закрыть":
-        break
     
     #английская версия / english version
+    #сайты / sites:
+    elif command == "youtube":
+        webbrowser.open('https://www.youtube.com')
+        
+    elif command == "telegram site":
+        webbrowser.open('https://web.telegram.org/')
+
+    elif command == "github":
+        webbrowser.open('https://github.com/')
+    
+    elif command == "github developer":
+        webbrowser.open('https://github.com/DewGit3')
+        
+    elif command == "chat GPT":
+        webbrowser.open('https://chatgpt.com/uc/6aa296e2-980c-83ea-9095-e25ceac01998')  
+        
+    elif command == "Claude":
+        webbrowser.open('https://claude.com/')
+        
+    elif command == "spotify":
+        webbrowser.open('https://open.spotify.com')
+
+
+    #приложения / apps    
     elif command == "chrome":
         os.system("start chrome")
         
     elif command == "firefox":
         os.system("start firefox")
         
-    elif command == "youtube":
-        webbrowser.open('www.youtube.com')
-        
     elif command == "discord":
         os.system("start discord:")
         
-    elif command == "telegram":
+    elif command == "telegram":  
         os.system("start tg:")
         
-    elif command == "tg":
+    elif command == "tg":        
         os.system("start tg:")
-        
-    elif command == "telegram browser":
-        webbrowser.open('https://web.telegram.org/')
     
     elif command == "steam":
         os.system("start steam:")
         
-    elif command == "github":
-        webbrowser.open('https://github.com/')
-        
-    elif command == "github developer":
-        webbrowser.open('https://github.com/DewGit3')
-        
     elif command == "console":
         os.system("start cmd")
     
-    elif command == "chat GPT":
-        webbrowser.open('https://chatgpt.com/uc/6aa296e2-980c-83ea-9095-e25ceac01998')
-                
-    elif command == "Claude":
-        webbrowser.open('https://claude.com/')
+    elif command == "vs code":  
+        os.system("start code")
+        
+    elif command == "code":     
+        os.system("start code")
+    
+    elif command == "visual studio":  
+        os.system("start devenv")
+    
+    elif command == "vs":            
+        os.system("start devenv")
         
     elif command == "calc":
         os.system("start calc")
@@ -109,8 +130,12 @@ while True:
         
     elif command == "explorer":
         os.system("start explorer")
+        
+    elif command == "spotify":
+        os.system("start spotify")
     
     
+    #конец / end
     elif command == "close":
         break
     elif command == "stop":
